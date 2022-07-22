9e05a9c9f0ce13325729d430377a51e559b39d53
Om Chikane <chikaneom007@gmail.com>
2022-07-22 13:55:23 +0530

    Added: Launch Browser Using Selenium

---
+++ src/main/java/CQA_105_Selenium_Java/CQA_105_Selenium_Java/App.java
@@ -0,0 +1,13 @@
+package CQA_105_Selenium_Java.CQA_105_Selenium_Java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/company/selenium_java/Launch_Browser.java
@@ -0,0 +1,39 @@
+package com.company.selenium_java;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Launch_Browser {
+	@Test
+	public void launch() throws InterruptedException{
+		System.setProperty("webdriver.gecko.driver","C:\\Selenium\\geckodriver-64bit\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();	
+		driver.get("http://www.facebook.com");
+		driver.manage().window().maximize();
+		System.out.println( driver.getTitle());
+		String pageTitle = driver.getTitle();
+		System.out.println("the title of the page is :"+ pageTitle);
+		Thread.sleep(2000);
+		String currentUrl = driver.getCurrentUrl();
+		System.out.println("the URL of the page is :"+ currentUrl);
+		Thread.sleep(2000);
+		System.out.println( driver.getPageSource());
+		driver.navigate().to("https://mail.google.com/mail/u/0/#inbox");
+		String pageTitle1 = driver.getTitle();
+		System.out.println("the title of the page is :"+ pageTitle1);
+		Thread.sleep(2000);
+		driver.navigate().back();
+		Thread.sleep(3000);
+		driver.navigate().refresh();
+		driver.close();
+		driver.navigate().forward();
+		Thread.sleep(3000);
+		driver.findElement(By.linkText("Create New Account")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.name("firstname")).sendKeys("Omkar");		
+//		driver.close();	
+	
+	}
+}
